67847562de474addcf5e8dc565df76db86bd092f
John Doe <johndoe@example.com>
2023-04-20 16:58:17 +0400

    i did the task 12

--- task12/task12.js
+++ task12/task12.js
@@ -1,3 +1,12 @@
-function iNeedHelp () {
-    alert ('Help :-(')
+// function iNeedHelp () {
+//     alert ('Help :-(')
+// }
+
+function sumOfDigits() {
+  let code = prompt("Enter your code:");
+  let sum = 0;
+  for (let i = 0; i < code.length; i++) {
+    sum += parseInt(code.charAt(i));
+  }
+  alert("the sum of digits is: " + sum);
 }
